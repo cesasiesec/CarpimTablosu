8a92ac8695ee2702fcb39cd8a6ce2a1c5920c0f9
Cesasie <Cesasie@DESKTOP-4F3U2I3>
2023-04-22 01:21:30 +0300

    Proje dosyası ekle.

---
+++ CarpimTablosu/CarpimTablosu.cpp
@@ -0,0 +1,18 @@
+﻿//ic ice donguler icin ornek
+//ekrana carpim tablosunu bastıran kodu yaziniz.
+#include <iostream>
+using namespace std;
+int main() {
+	int boyut;
+	cout << "Kac sutunlu bir carpim tablosu istiyorsunuz?:";
+	cin >> boyut;
+	for (int i = 1; i <= boyut; i++) {
+		for (int j = 1; j <= boyut; j++)
+		{
+			cout << i * j << " , ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
